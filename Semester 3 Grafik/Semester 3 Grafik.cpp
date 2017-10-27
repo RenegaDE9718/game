91d7381a21cf3cfc924dd95bd5a498e92f478801
RenegaDE9718 <robin.prosi@gmail.com>
2017-10-27 13:48:13 +0200

    Plattform Klasse hinzugefügt

--- Semester 3 Grafik/Semester 3 Grafik.cpp
+++ Semester 3 Grafik/Semester 3 Grafik.cpp
@@ -5,20 +5,63 @@
 #include <Gosu/Gosu.hpp>
 #include <Gosu/AutoLink.hpp>
 
-const int Fensterhoehe = 600;
-const int Fensterbreite = 800;
+
+const int windowHeight = 600;
+const int windowWidth = 800;
+
 bool left;
 bool right;
 bool jump;
+
 int gravity = 10;
-int distance = 0;
+int distance = 0;		// Zählvariable für die Sprunghöhe
 int jumpHeight = 300;	// Festgelegte Sprunghöhe
-double xPos = Fensterbreite/2;
-double yPos = Fensterhoehe/2;
+double xPos = windowWidth/2;
+double yPos = windowHeight/2;
+
+
+
+class platform {
+
+	double xPlat;
+	double yPlat;
+	double height = 10;
+	double width = 100;
+
+public:
+
+	void setX(double x) {
+		xPlat = x;
+	}
+
+	double getX() {
+		return xPlat;
+	}
+
+	void setY(double y) {
+		yPlat = y;
+	}
+
+	double getY() {
+		return yPlat;
+	}
+
+	double getHeight() {
+		return height;
+	}
+
+	double getWidth() {
+		return width;
+	}
+
+};
+
 
 
 class GameWindow : public Gosu::Window {
+
 public:
+
 	Gosu::Image bild;
 	GameWindow()
 		: Window(640, 480)
@@ -32,19 +75,19 @@ public:
 		left = input().down(Gosu::ButtonName::KB_LEFT);
 		right = input().down(Gosu::ButtonName::KB_RIGHT);
 
-		if (left == 1) {
+		if (left == 1) {		// Bewegt die Spielfigur nach links
 			xPos -= 5;
 		}
 
-		if (right == 1) {
+		if (right == 1) {		// Bewegt die Spielfigur nach rechts
 			xPos += 5;
 		}
 
-		if (yPos + bild.height() <= Fensterhoehe && jump == 0) { // Schwerkraft, wirkt erst nach erreichen der maximalen Sprunghöhe
+		if (yPos + bild.height() <= windowHeight && jump == 0) { // Schwerkraft, wirkt erst nach erreichen der maximalen Sprunghöhe
 			yPos += gravity;
 		}
 
-		if (yPos + bild.height() == Fensterhoehe) { // Sprungvariable setzen
+		if (yPos + bild.height() == windowHeight) { // Sprungvariable setzen
 			jump = true;
 		}
 
@@ -59,26 +102,35 @@ public:
 		}
 
 		if (xPos < 0) {			// Verlassen des Bildschirms nach links und dann wechsel nach rechts
-			xPos = Fensterbreite + xPos - bild.width();
+			xPos = windowWidth + xPos - bild.width();
 		}
 
-		if (xPos + bild.width() > Fensterbreite) {		// Verlassen des Bildschirms nach rechts und dann wechsel nach links
-			xPos = Fensterbreite - xPos - bild.width();
+		if (xPos + bild.width() > windowWidth) {		// Verlassen des Bildschirms nach rechts und dann wechsel nach links
+			xPos = windowWidth - xPos - bild.width();
 		}
+
+		
 	}
 
 	void draw() override {
 
-		bild.draw_rot(xPos, yPos, 0.0, 0.0, 1.0, 1.0
-
-		);
+		platform p;
+		p.setX(100);
+		p.setY(100);
+		bild.draw_rot(xPos, yPos, 0.0, 0.0, 1.0, 1.0);
+		graphics().draw_quad(p.getX(),p.getY(),Gosu::Color::GREEN,
+							p.getX() + p.getWidth(), p.getY(), Gosu::Color::GREEN,
+							p.getX(), p.getY() + p.getHeight(), Gosu::Color::GREEN,
+							p.getX() + p.getWidth(), p.getY() + p.getHeight(), Gosu::Color::GREEN, 0.0);
+		
 
 	}
 };
 
+
 int main()
 {
-GameWindow window;
-window.show();
+	GameWindow window;
+	window.show();
 
 }
